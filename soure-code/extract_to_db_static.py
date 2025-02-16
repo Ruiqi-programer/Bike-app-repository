1a1ad72d353278e6c1ee99974798580cafce08a0
Shuangning Wei <shuangning.wei@ucdconnect.ie>
2025-02-16 10:55:27 +0000

    station_upate

--- soure-code/extract_to_db_static.py
+++ soure-code/extract_to_db_static.py
@@ -8,6 +8,36 @@ import json
 from sqlalchemy import create_engine,text
 import pymysql
 
+
+#create tables
+def create_station_table(in_engine):
+    sql = text('''
+    CREATE TABLE IF NOT EXISTS station (
+    number INTEGER PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    address VARCHAR(256), 
+    position_lat DOUBLE NOT NULL,
+    position_lng DOUBLE NOT NULL,
+    bonus BOOLEAN ,
+    banking BOOLEAN ,
+    bikestands INTEGER NOT NULL
+    
+    );
+    ''')
+ 
+    with in_engine.connect() as conn:
+        conn.execute(text("DROP TABLE IF EXISTS station;"))
+        #create tables
+        conn.execute(sql)
+        # Use the engine to execute the DESCRIBE command to inspect the table schema
+        tab_structure=conn.execute(text("SHOW COLUMNS FROM station;"))
+
+        # Fetch and print the result to see the columns of the table
+        columns = tab_structure.fetchall()
+        print(columns)
+
+
+
 def stations_to_db(text_data,in_engine):
     # let us load the stations from the text received from jcdecaux
     stations = json.loads(text_data)
@@ -38,8 +68,8 @@ def stations_to_db(text_data,in_engine):
                         "address": station.get("address", "Unknown"),
                         "position_lat":position.get("lat",0.0),
                         "position_lng":position.get("lng",0.0),
-                        "bonus": bool(station.get("bonus", False)),
-                        "banking": bool(station.get("banking", False)),
+                        "bonus": bool(station.get("bonus")),
+                        "banking": bool(station.get("banking")),
                         "bikestands": int(station.get("bike_stands", 0))
                     }
                 conn.execute(insert,values)
@@ -55,11 +85,11 @@ def main():
     URI = "localhost"
 
     connection_string = "mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB)
-
     engine = create_engine(connection_string, echo = True)
 
 
     try:
+        create_station_table(engine)
         r = requests.get(dbinfo.STATIONS_URI, params={"apiKey": dbinfo.JCKEY, "contract": dbinfo.NAME})
         stations_to_db(r.text,engine)
     except:
